9d02c78ba185d5376b411acea87d83e4acdc96a3
BtwToxic <btwalexxd@gmail.com>
2025-12-31 23:45:23 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,10 +1,26 @@
-setInterval(()=>{
- fetch("/api/logs/"+sid).then(r=>r.json()).then(d=>{
-   document.getElementById("l").innerText=d.join("\n")
- })
- fetch("/api/metrics/"+sid).then(r=>r.json()).then(m=>{
-   chart.data.datasets[0].data.push(m.cpu||0)
-   chart.data.datasets[1].data.push(m.memory||0)
-   chart.update()
- })
-},5000)
+const sid = window.serviceId
+
+function showLoader(show=true){
+  document.getElementById("loader").style.display = show?"block":"none"
+}
+
+function refreshAll(){
+  showLoader(true)
+
+  fetch("/api/logs/"+sid)
+    .then(r=>r.json())
+    .then(d=>{
+      document.getElementById("logs").innerText = d.join("\n")
+    })
+
+  fetch("/api/metrics/"+sid)
+    .then(r=>r.json())
+    .then(m=>{
+      cpu.data.datasets[0].data.push(m.cpu||0)
+      ram.data.datasets[0].data.push(m.memory||0)
+      cpu.update(); ram.update()
+      showLoader(false)
+    })
+}
+
+setInterval(refreshAll,5000)
