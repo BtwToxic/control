07a2a2350e4b57e9b1a1f59ac2d0a10a7a2ad502
BtwToxic <btwalexxd@gmail.com>
2025-12-31 23:37:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+SUPER_ADMIN = "dev"
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
+RAILWAY_API_KEY = os.getenv("RAILWAY_API_KEY", "")
